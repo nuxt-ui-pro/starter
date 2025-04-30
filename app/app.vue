a29ca870a170fc147a94a658f64593422a60780b
Benjamin Canac <canacb1@gmail.com>
2025-04-30 11:51:13 +0200

    chore(app): update title

--- app/app.vue
+++ app/app.vue
@@ -11,7 +11,7 @@ useHead({
   }
 })
 
-const title = 'Nuxt UI Pro - Starter'
+const title = 'Nuxt UI Pro Starter Template'
 const description = 'Nuxt UI Pro is a collection of premium Vue components built on top of Nuxt UI to create beautiful & responsive Nuxt applications in minutes.'
 
 useSeoMeta({
