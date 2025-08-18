c91ac6375f99d4cf688a8e77118767e31db56090
Hugo Richard <hugo.richard@epitech.eu>
2025-08-18 11:47:23 +0100

    up

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -11,7 +11,7 @@
         size: 'xl'
       }, {
         label: 'Use this template',
-        to: 'https://github.com/nuxt-ui/starter',
+        to: 'https://github.com/nuxt-ui-pro/starter',
         target: '_blank',
         icon: 'i-simple-icons-github',
         size: 'xl',
@@ -52,7 +52,7 @@
           color: 'neutral'
         }, {
           label: 'GitHub',
-          to: 'https://github.com/nuxt/ui',
+          to: 'https://github.com/nuxt-ui-pro/starter',
           target: '_blank',
           trailingIcon: 'i-simple-icons-github',
           color: 'neutral',
